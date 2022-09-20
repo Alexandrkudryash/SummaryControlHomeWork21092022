dc2978359ca37d6390dc771fc1521a369efa4cab
Alexander <alexandrkudryash@mail.ru>
2022-09-20 19:27:37 +0300

    added Code in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,57 +1,44 @@
 ﻿// ИТОГОВАЯ ПРОВЕРОЧНАЯ РАБОТА
 
+Console.Clear();
+
+string [] ChangeArray(string[] array, int TheLongestWord)
+{
+    string [] changedArray = {};
+    for (int i = 0; i <array.Length; i++)
+    {
+        if (array[i].Length <= TheLongestWord)
+        {
+            Array.Resize(ref changedArray, changedArray.Length+1);
+            changedArray[changedArray.Length-1] = array[i];
+        }
+    }
+    return changedArray;
+}
+
+void PrintArray(string[] array)
+{
+  int i = 0;
+  while (i < array.Length)
+  {
+    Console.Write(array[i] +", ");
+    i++;
+  }
+}
+
 string [] text1 = 
   {"фон", "Африка", "пример", "156","вот","Сон","ехать","17567",
    "дом","он", "здесь","так", "визит", "птица", "7866", "абрикос",
    "мех", "черника", "КодНеполучается", "как", "?"};
 
-Console.WriteLine($"Размер заданного массива: " +$"{text1.Length}");
-
-void FillArray(string[] collection)
-{
-    int length = collection.Length;
-    int index = 0;
-    while(index < length)
-    {
-        if (text1[index].Length <=3 && text1[index] !="")
-        collection[index]= text1[index];
-        index++;
-    }
-}
-
-
-void PrintArray(string[] col)
-{
-  int count = col.Length;
-  int position = 0;
-  while (position < count)
-  {
-    //if(col[position].Length <=3)
-    Console.Write(col[position] +", ");
-    position++;
-  }
-}
-
-
-int size = 0;
-for (int i = 0; i < text1.Length; i++)
-{
-if (text1[i].Length <= 3)
-   {
-    size = size+1;
-   }
-}
-Console.WriteLine("Размер для нового массива из слов меньше либо");
-Console.WriteLine("равно 3 символам составляет: " +$"{size}");
-
-Console.WriteLine("Задан такой массив");
+Console.WriteLine("Задан такой массив строк:");
 PrintArray(text1);
-
 Console.WriteLine();
 
-string[] newtext = new string[text1.Length];
-Console.WriteLine("Преобразуем в такой массив из строк <=3:");
-FillArray(newtext);
-PrintArray(newtext);
-Console.WriteLine();
+var action = ChangeArray(text1, 3);
 
+Console.WriteLine("Преобразуем в такой массив из строк <= 3:");
+
+PrintArray(action);
+Console.WriteLine();
+// результат: фон, 156, вот, Сон, дом, он, так, мех, как, ?,
